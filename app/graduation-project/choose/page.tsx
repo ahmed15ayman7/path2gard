7d7cd7fd6c9bc610e5f4ca5d9a378b59134b42c3
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-02-21 07:26:46 +0200

    first commit

--- app/graduation-project/choose/page.tsx
+++ app/graduation-project/choose/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 import ProjectsBank from "@/components/project/ProjectsBank";
 
-export default function ProjectBankPage() {
-  return <ProjectsBank />;
+function ProjectBankPage() {
+  const searchParams = useSearchParams();
+  
+  return (
+    <div>
+      <ProjectsBank />
+    </div>
+  );
+}
+
+export default function ProjectBankPageWrapper() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ProjectBankPage />
+    </Suspense>
+  );
 }
